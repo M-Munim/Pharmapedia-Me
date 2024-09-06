aca2777fe0a923d5b7007984437404eed48e3c65
munim <muhammadmunimoff330@gmail.com>
2024-09-06 14:37:14 +0500

    changes in path nav logout

--- src/app/Dashboard/Home/page.js
+++ src/app/Dashboard/Home/page.js
@@ -3,7 +3,7 @@
 import { Bar, Pie, Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement, PointElement, LineElement } from 'chart.js';
 import HomeLayout from '../Components/HomeLayout';
-import useTokenRedirect from '@/middleware';
+import useTokenRedirect from '@/middleware.js';
 
 
 // Register the required components
