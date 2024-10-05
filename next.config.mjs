f1b16bfd2ac387d47ade798243b00f09244ce564
munim <muhammadmunimoff330@gmail.com>
2024-10-05 12:59:14 +0500

    testing google analytics

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    analyticsId: 'G-GY7RKNSGB4',  // Replace with your Google Analytics Measurement ID
+  },
+};
 
 export default nextConfig;
